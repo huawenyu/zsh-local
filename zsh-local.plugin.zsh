b7d95ffbb51bc013dff80d0e67f87bf0f3af8223
Huawen Yu <hyu@fortinet.com>
2020-10-11 21:08:05 -0700

    update wiki

--- zsh-local.plugin.zsh
+++ zsh-local.plugin.zsh
@@ -333,16 +333,20 @@ if [ -d "/bin" ]; then
   export PATH="/bin:$PATH"
 fi
 
-if [ -d "$HOME/bin" ]; then
-  export PATH="$HOME/bin:$PATH"
-fi
-
 if [ -d "/usr/local/sbin" ]; then
   export PATH="/usr/local/sbin:$PATH"
 fi
 
 if [ -d "/usr/sbin" ]; then
   export PATH="/usr/sbin:$PATH"
+fi
+
+if [ -d "$HOME/.local/bin" ]; then
+  export PATH="$HOME/.local/bin:$PATH"
+fi
+
+if [ -d "$HOME/bin" ]; then
+  export PATH="$HOME/bin:$PATH"
 fi
 
 #parse_git_branch() {
