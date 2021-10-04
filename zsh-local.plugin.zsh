28b8385e18638501c927c869ecfaf7d99a71bc84
Huawen Yu <hyu@fortinet.com>
2021-10-04 10:27:49 -0700

    update

--- zsh-local.plugin.zsh
+++ zsh-local.plugin.zsh
@@ -1238,3 +1238,48 @@ export MANWIDTH=999
 ##   cd nvimpager && make PREFIX=$HOME/.local install
 ##export MANPAGER=nvimpager
 
+
+
+# tmux-zsh-environment.plugin
+# https://github.com/MikeDacre/tmux-zsh-environment/blob/master/tmux-zsh-environment.plugin.zsh
+#
+#   git clone https://github.com/MikeDacre/tmux-zsh-environment.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/tmux-zsh-environment
+#       Share environmental variables more seamlessly between tmux and ZSH
+#       https://github.com/MikeDacre/tmux-zsh-environment
+#       Usage:
+#          Publish a var: $ tmux set-env name2 wilson2
+#          Check at another shell (<enter> to tigger hook-cmd): $ echo $name2
+#######################################################################
+#                    Tmux ZSH Environment Sharing                     #
+#######################################################################
+# Don't run if @delphij's version is running
+[[ -n "$ZSH_TMUX_AUTOREFRESH" ]] || ZSH_TMUX_AUTOREFRESH=false
+
+if "$ZSH_TMUX_AUTOREFRESH"; then
+    return
+fi
+
+# Control running with TMUX_ZSH_SYNC
+[[ -n "$TMUX_ZSH_SYNC" ]] || TMUX_ZSH_SYNC=true
+
+if ! "$TMUX_ZSH_SYNC"; then
+    return
+fi
+
+# Do nothing at all if we are not in tmux, second test is needed in
+# case of an ssh session being run from within tmux, which could result
+# in $TMUX from the parent session being set without tmux being
+# connected.
+if [ -n "$TMUX" ] && tmux ls >/dev/null 2>/dev/null; then
+    # Update shell environment from variables, don't unset
+    _tmux_zsh_env__preexec() {
+        eval "$(tmux show-environment -s | grep -v "^unset")"
+    }
+
+    # Add the function as a precmd, to be run at every new prompt
+    autoload -Uz add-zsh-hook
+    #add-zsh-hook precmd _zsh_title__preexec
+    add-zsh-hook precmd _tmux_zsh_env__preexec
+fi
+#######################################################################
+
