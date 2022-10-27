eaa506223d8dc9a98227a3b550a324def4de56fb
Huawen Yu <hyu@fortinet.com>
2022-10-27 13:11:22 -0700

    update vim has('cscope')

--- zsh-local.plugin.zsh
+++ zsh-local.plugin.zsh
@@ -199,8 +199,22 @@ fi
 alias pwd=" pwd | sed 's/^/ /g'"
 #alias pwd=' pwd -L'
 
-# sudo apt install -y tldr
+## sudo apt install -y tldr
+# Better-support customize dir: pip3 install tldr
 is-callable tldr && { alias m=tldr; }
+if is-callable tldr; then
+    #export TLDR_COLOR_NAME="cyan"
+    #export TLDR_COLOR_DESCRIPTION="white"
+    #export TLDR_COLOR_EXAMPLE="green"
+    #export TLDR_COLOR_COMMAND="red"
+    #export TLDR_COLOR_PARAMETER="white"
+    #export TLDR_LANGUAGE="es"
+    #export TLDR_CACHE_ENABLED=1
+    #export TLDR_CACHE_MAX_AGE=720
+    #export TLDR_PAGES_SOURCE_LOCATION="https://raw.githubusercontent.com/tldr-pages/tldr/master/pages"
+    export TLDR_PAGES_SOURCE_LOCATION=file://$HOME/dotfiles/tldr
+    export TLDR_DOWNLOAD_CACHE_LOCATION="https://tldr-pages.github.io/assets/tldr.zip"
+fi
 
 ## pip3 install thefuck --user
 #if command -v thefuck &> /dev/null; then
