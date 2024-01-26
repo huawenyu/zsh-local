15ed39c7a8c159b215474c5aead0e5c6d0830253
Huawen Yu <hyu@fortinet.com>
2024-01-26 07:23:49 -0800

    update

--- zsh-local.plugin.zsh
+++ zsh-local.plugin.zsh
@@ -219,7 +219,7 @@ alias mysudo='sudo -E env "PATH=$PATH"'
 
 ## sudo apt install -y tldr
 # Better-support customize dir: pip3 install tldr
-is-callable tldr && { alias m="tldr -p linux common -L en "; }
+is-callable tldr && { alias m="tldr -p linux common -L en" }
 is-callable tldr && is-callable fzf && { alias tl="tldr --list | fzf --preview 'tldr {}' | xargs tldr" }
 if is-callable tldr; then
     #export TLDR_COLOR_NAME="cyan"
